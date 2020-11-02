506eba7ae8d48bbdbee7248d5999c52120b4193f
Muligheder <anders.pedersen7@gmail.com>
2020-11-02 10:28:02 +0100

    Rotation is now correct but translation is wrong in final result

--- Test_program/main.cpp
+++ Test_program/main.cpp
@@ -1059,6 +1059,7 @@ Mat eulerAnglesToRotationMatrix(Vec3f &theta);
 Vec3f rotationMatrixToEulerAngles(Mat &R);
 float rad2deg(float radian);
 float deg2rad(float degree);
+Mat ReverseVector(Mat &v);
 
 int main()
 {
@@ -1093,14 +1094,14 @@ int main()
     // Camera calibration information
 
     std::vector<double> distortionCoefficients(5);  // camera distortion
-    distortionCoefficients[0] = 2.4472856611074989e-01;
-    distortionCoefficients[1] = -8.1042032574246325e-01;
+    distortionCoefficients[0] = 9.6349551984637724e-02;
+    distortionCoefficients[1] = -3.3260675111130217e-01;
     distortionCoefficients[2] = 0;
     distortionCoefficients[3] = 0;
-    distortionCoefficients[4] = 7.8769462320821060e-01;
+    distortionCoefficients[4] = 2.5833277679122602e-01;
 
-    double f_x = 1.3624172121852105e+03; // Focal length in x axis
-    double f_y = 1.3624172121852105e+03; // Focal length in y axis (usually the same?)
+    double f_x = 1.2993539019658076e+03; // Focal length in x axis
+    double f_y = 1.2993539019658076e+03; // Focal length in y axis (usually the same?)
     double c_x = 960; // Camera primary point x
     double c_y = 540; // Camera primary point y
 
@@ -1144,8 +1145,9 @@ int main()
     for (int i = 0; i < patternsize.height; ++i)
         for (int j = 0; j < patternsize.width; ++j)
             obj_points.push_back(Point3f(float(j*cell_size),
-                float(i*cell_size), 0.f));
-
+                                         float(i*cell_size), 0.f));
+cout <<"Objectpoints: " <<endl<<obj_points<<endl;
+cout <<"Objectpoints: " <<endl<<obj_points.size()<<endl;
     for (size_t i = 0; i < num_images; i++)
         images.push_back(imread(fn[i]));
 
@@ -1158,21 +1160,22 @@ int main()
         Mat gray;
         cvtColor(frame,gray,COLOR_BGR2GRAY);
 
-      //  imshow("window",gray);
-       // waitKey(0);
+        //  imshow("window",gray);
+        // waitKey(0);
 
-        bool patternfound = findChessboardCorners(frame, patternsize, centers,CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
-                                                  + CALIB_CB_FAST_CHECK);
+        bool patternfound = findChessboardCorners(frame, patternsize, centers);
         if (patternfound)
         {
-            //cornerSubPix(frame, centers, Size(11, 11), Size(-1, -1),
-              //                   TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+          //  cornerSubPix(gray, centers, Size(1, 1), Size(-1, -1),
+            //                  TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
 
             drawChessboardCorners(frame, patternsize, Mat(centers), patternfound);
-            imshow("window", frame);
-            int key = cv::waitKey(0) & 0xff;
-            solvePnP(Mat(obj_points), Mat(centers), cameraMatrix, distortionCoefficients, rvec, tvec);
-            cout <<"Rotation vetor = "<<" " <<endl<<endl<<rvec;
+            // imshow("window", frame);
+           //int key = cv::waitKey(0) & 0xff;
+            solvePnP(Mat(obj_points), Mat(centers), cameraMatrix, distortionCoefficients, rvec, tvec,false,SOLVEPNP_ITERATIVE);
+           // cout <<"Rotation vetor = "<<endl<<" " <<rvec<<endl<<endl;
+
+
             Mat R;
             Rodrigues(rvec, R); // R is 3x3
             R_target2cam.push_back(R);
@@ -1188,16 +1191,16 @@ int main()
     }
 
     //EULER=ZYX
-//    Vec3f theta_01{ deg2rad(-153.61), deg2rad(8.3),   deg2rad(-91.91) };
-//    Vec3f theta_02{ deg2rad(-166.71), deg2rad(3.04),  deg2rad(-93.31) };
-//    Vec3f theta_03{ deg2rad(-170.04), deg2rad(24.92), deg2rad(-88.29) };
-//    Vec3f theta_04{ deg2rad(-165.71), deg2rad(24.68), deg2rad(-84.85) };
-//    Vec3f theta_05{ deg2rad(-160.18), deg2rad(-15.94),deg2rad(-56.24) };
-//    Vec3f theta_06{ deg2rad(175.68),  deg2rad(10.95), deg2rad(180) };
-//    Vec3f theta_07{ deg2rad(175.73),  deg2rad(45.78), deg2rad(-179.92) };
-//    Vec3f theta_08{ deg2rad(-165.34), deg2rad(47.37), deg2rad(-166.25) };
-//    Vec3f theta_09{ deg2rad(-165.62), deg2rad(17.95), deg2rad(-166.17) };
-//    Vec3f theta_10{ deg2rad(-151.99), deg2rad(-14.59),deg2rad(-94.19) };
+    //    Vec3f theta_01{ deg2rad(-153.61), deg2rad(8.3),   deg2rad(-91.91) };
+    //    Vec3f theta_02{ deg2rad(-166.71), deg2rad(3.04),  deg2rad(-93.31) };
+    //    Vec3f theta_03{ deg2rad(-170.04), deg2rad(24.92), deg2rad(-88.29) };
+    //    Vec3f theta_04{ deg2rad(-165.71), deg2rad(24.68), deg2rad(-84.85) };
+    //    Vec3f theta_05{ deg2rad(-160.18), deg2rad(-15.94),deg2rad(-56.24) };
+    //    Vec3f theta_06{ deg2rad(175.68),  deg2rad(10.95), deg2rad(180) };
+    //    Vec3f theta_07{ deg2rad(175.73),  deg2rad(45.78), deg2rad(-179.92) };
+    //    Vec3f theta_08{ deg2rad(-165.34), deg2rad(47.37), deg2rad(-166.25) };
+    //    Vec3f theta_09{ deg2rad(-165.62), deg2rad(17.95), deg2rad(-166.17) };
+    //    Vec3f theta_10{ deg2rad(-151.99), deg2rad(-14.59),deg2rad(-94.19) };
 
     //Part2
     Vec3f theta_01{ deg2rad(-37.2789), deg2rad(89.5008),   deg2rad(144.7559) };
@@ -1224,28 +1227,44 @@ int main()
     Mat robot_rot_09 = eulerAnglesToRotationMatrix(theta_09);
     Mat robot_rot_10 = eulerAnglesToRotationMatrix(theta_10);
 
-//    const Mat robot_tr_01 = (Mat_<float>(3, 1) << 781.2, 338.59, 903.48);
-//    const Mat robot_tr_02 = (Mat_<float>(3, 1) << 867.65, 382.52, 884.42);
-//    const Mat robot_tr_03 = (Mat_<float>(3, 1) << 856.91, 172.99, 964.61);
-//    const Mat robot_tr_04 = (Mat_<float>(3, 1) << 748.81, 146.75, 1043.29);
-//    const Mat robot_tr_05 = (Mat_<float>(3, 1) << 627.66, 554.08, 920.85);
-//    const Mat robot_tr_06 = (Mat_<float>(3, 1) << 715.06, 195.96, 889.38);
-//    const Mat robot_tr_07 = (Mat_<float>(3, 1) << 790.9, 196.29, 1117.38);
-//    const Mat robot_tr_08 = (Mat_<float>(3, 1) << 743.5, 283.93, 1131.92);
-//    const Mat robot_tr_09 = (Mat_<float>(3, 1) << 748.9, 288.19, 910.58);
-//    const Mat robot_tr_10 = (Mat_<float>(3, 1) << 813.18, 400.44, 917.16);
+    //    const Mat robot_tr_01 = (Mat_<float>(3, 1) << 781.2, 338.59, 903.48);
+    //    const Mat robot_tr_02 = (Mat_<float>(3, 1) << 867.65, 382.52, 884.42);
+    //    const Mat robot_tr_03 = (Mat_<float>(3, 1) << 856.91, 172.99, 964.61);
+    //    const Mat robot_tr_04 = (Mat_<float>(3, 1) << 748.81, 146.75, 1043.29);
+    //    const Mat robot_tr_05 = (Mat_<float>(3, 1) << 627.66, 554.08, 920.85);
+    //    const Mat robot_tr_06 = (Mat_<float>(3, 1) << 715.06, 195.96, 889.38);
+    //    const Mat robot_tr_07 = (Mat_<float>(3, 1) << 790.9, 196.29, 1117.38);
+    //    const Mat robot_tr_08 = (Mat_<float>(3, 1) << 743.5, 283.93, 1131.92);
+    //    const Mat robot_tr_09 = (Mat_<float>(3, 1) << 748.9, 288.19, 910.58);
+    //    const Mat robot_tr_10 = (Mat_<float>(3, 1) << 813.18, 400.44, 917.16);
 
     //Part 2
-    const Mat robot_tr_01 = (Mat_<float>(3, 1) << -941.7910, 8.6871, 120.6610);
-    const Mat robot_tr_02 = (Mat_<float>(3, 1) << -947.4020, -153.0410, 101.1080);
-    const Mat robot_tr_03 = (Mat_<float>(3, 1) << -950.4640, -150.7550, 537.3240);
-    const Mat robot_tr_04 = (Mat_<float>(3, 1) << -944.2820, 22.7132, 536.4470);
-    const Mat robot_tr_05 = (Mat_<float>(3, 1) << -1197.3, 69.3, 335.5);
-    const Mat robot_tr_06 = (Mat_<float>(3, 1) << -1076.7, -104.6, 323.4);
-    const Mat robot_tr_07 = (Mat_<float>(3, 1) << -1162.7, -110.8, 396.7);
-    const Mat robot_tr_08 = (Mat_<float>(3, 1) << -1073.8, -128.3, 438.5);
-    const Mat robot_tr_09 = (Mat_<float>(3, 1) << -1073.8, -128.3, 438.5);
-    const Mat robot_tr_10 = (Mat_<float>(3, 1) << -1171.5, -166.3, 630.0);
+     Mat robot_tr_01 = (Mat_<float>(3, 1) << -941.7910, 8.6871, 120.6610);
+     Mat robot_tr_02 = (Mat_<float>(3, 1) << -947.4020, -153.0410, 101.1080);
+     Mat robot_tr_03 = (Mat_<float>(3, 1) << -950.4640, -150.7550, 537.3240);
+     Mat robot_tr_04 = (Mat_<float>(3, 1) << -944.2820, 22.7132, 536.4470);
+     Mat robot_tr_05 = (Mat_<float>(3, 1) << -1197.3, 69.3, 335.5);
+     Mat robot_tr_06 = (Mat_<float>(3, 1) << -1076.7, -104.6, 323.4);
+     Mat robot_tr_07 = (Mat_<float>(3, 1) << -1162.7, -110.8, 396.7);
+     Mat robot_tr_08 = (Mat_<float>(3, 1) << -1073.8, -128.3, 438.5);
+     Mat robot_tr_09 = (Mat_<float>(3, 1) << -1073.8, -128.3, 438.5);
+     Mat robot_tr_10 = (Mat_<float>(3, 1) << -1171.5, -166.3, 630.0);
+
+//     //Reverse the translation vector from XYZ to ZYX
+//     robot_tr_01= ReverseVector(robot_tr_01);
+//     robot_tr_02= ReverseVector(robot_tr_02);
+//     robot_tr_03= ReverseVector(robot_tr_03);
+//     robot_tr_04= ReverseVector(robot_tr_04);
+//     robot_tr_05= ReverseVector(robot_tr_05);
+//     robot_tr_06= ReverseVector(robot_tr_06);
+//     robot_tr_07= ReverseVector(robot_tr_07);
+//     robot_tr_08= ReverseVector(robot_tr_08);
+//     robot_tr_09= ReverseVector(robot_tr_09);
+//     robot_tr_10= ReverseVector(robot_tr_10);
+
+
+//     cout << "Robot_tr_011 = " << endl << " " << ReverseVector(robot_tr_01) << endl << endl;
+
 
 
     R_gripper2base.push_back(robot_rot_01);
@@ -1282,11 +1301,11 @@ int main()
     cout << R_target2cam.size() << " R_target2cam"<<endl;
     cout << R_target2cam.size() << " t_target2cam"<<endl;
     cout << R_cam2gripper.size() << " R_cam2gripper"<<endl;
-    cout << t_cam2gripper.size() << " t_cam2gripper"<<endl;
+    cout << t_cam2gripper.size() << " t_cam2gripper"<<endl<<endl;
 
 
 
-    calibrateHandEye(R_gripper2base, t_gripper2base, R_target2cam, t_target2cam, R_cam2gripper, t_cam2gripper, CALIB_HAND_EYE_PARK);
+    calibrateHandEye(R_gripper2base, t_gripper2base, R_target2cam, t_target2cam, R_cam2gripper, t_cam2gripper, CALIB_HAND_EYE_TSAI);
 
     Vec3f R_cam2gripper_r = rotationMatrixToEulerAngles(R_cam2gripper);
 
@@ -1300,8 +1319,8 @@ Mat eulerAnglesToRotationMatrix(Vec3f &theta)
     // Calculate rotation about x axis
     Mat R_x = (Mat_<double>(3, 3) <<
         1, 0, 0,
-        0, cos(theta[0]), -sin(theta[0]),
-        0, sin(theta[0]), cos(theta[0])
+        0, cos(theta[2]), -sin(theta[2]),
+        0, sin(theta[2]), cos(theta[2])
         );
 
     // Calculate rotation about y axis
@@ -1313,10 +1332,9 @@ Mat eulerAnglesToRotationMatrix(Vec3f &theta)
 
     // Calculate rotation about z axis
     Mat R_z = (Mat_<double>(3, 3) <<
-        cos(theta[2]), -sin(theta[2]), 0,
-        sin(theta[2]), cos(theta[2]), 0,
+        cos(theta[0]), -sin(theta[0]), 0,
+        sin(theta[0]), cos(theta[0]), 0,
         0, 0, 1);
-
 
     // Combined rotation matrix
     Mat R = R_z * R_y * R_x;
@@ -1370,8 +1388,19 @@ Vec3f rotationMatrixToEulerAngles(Mat &R)
         y = atan2(-R.at<double>(2, 0), sy);
         z = 0;
     }
-    return Vec3f(rad2deg(z), rad2deg(y), rad2deg(x));
+    return Vec3f(rad2deg(x), rad2deg(y), rad2deg(z));
 
 
 }
 
+Mat ReverseVector(Mat &v)
+{
+
+    Mat RV = Mat_<float>(3,1);
+
+    RV.at<float>(0,0)=v.at<float>(0,2);
+    RV.at<float>(0,1)=v.at<float>(0,1);
+    RV.at<float>(0,2)=v.at<float>(0,0);
+
+    return RV;
+}
